dbe0767ddc2b3acdc58a6aad8717d91aa3258300
ankitm123 <ankitmohapatra123@gmail.com>
2022-08-29 12:12:48 -0400

    refactor: upgrade sveltekit to 1.0.0-next.375
    - Also add vite as a dev dependency
    
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

---
+++ web/vite.config.js
@@ -0,0 +1,18 @@
+import { sveltekit } from '@sveltejs/kit/vite'
+import { resolve } from 'path'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $src: resolve('./src'),
+      $lib: resolve('./src/lib'),
+      $stores: resolve('./src/stores'),
+      $assets: resolve('./src/assets'),
+      $icon: resolve('./node_modules/svelte-bootstrap-icons/lib'),
+    },
+  },
+}
+
+export default config
