b54044d7d97d0e8d54abfed4fef039616f6ca877
Christoffer Vig <christoffer.vig@gmail.com>
2022-11-06 23:50:49 +0100

    chore: use .env.production for production build env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM alpine:3.16.2
 COPY ./build/linux/jx-ui /usr/bin/jx-ui
 COPY ./web/build web/build
-COPY ./web/.env-prod ./web/.env
 ENTRYPOINT ["jx-ui"]
--- web/src/routes/admin/repositories/+page.ts
+++ web/src/routes/admin/repositories/+page.ts
@@ -1,10 +1,11 @@
 import { error } from '@sveltejs/kit'
+import { PUBLIC_BASE_PATH } from '$env/static/public'
 
 // Todo: move it to endpoints
 export async function load({ fetch }) {
   // ToDo: Use Axios
   const config = { method: 'get', headers: { origin: 'http://localhost:3000' } }
-  const request = new Request('/api/v1/repositories', config)
+  const request = new Request(`${PUBLIC_BASE_PATH}/api/v1/repositories`, config)
   const res = await fetch(request)
   const repositories = await res.json()
   if (res.ok) {
